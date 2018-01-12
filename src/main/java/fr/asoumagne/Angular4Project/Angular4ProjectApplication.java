336bc0c45cf2e4d06a52b103b39735f6b7587b10
unknown <alain21218@gmail.com>
2018-01-12 21:44:18 +0100

    first commit

---
+++ src/main/java/fr/asoumagne/Angular4Project/Angular4ProjectApplication.java
@@ -0,0 +1,12 @@
+package fr.asoumagne.Angular4Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular4ProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/asoumagne/Angular4Project/Angular4ProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.asoumagne.Angular4Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Angular4ProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
